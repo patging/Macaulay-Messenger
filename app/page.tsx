bd23a79f9dbef5527a8c31ea51972a0cf130bc0b
patging <patrickging04@gmail.com>
2022-12-27 14:54:29 -0500

    Components folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import Header from "../components/Header"
+
 // just here to test new file structure
 export default function page() {
-    return (
-        <h1>Hello, World!</h1>
+    return ( 
+        <>
+        <Header />
+        <h1 className="font-sans hover:font-serif">Hello, World!</h1>        
+        </>
+
     );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+import styles from "Header.module.css"
+
+
+const Header = () => {
+    return (
+        <div className=''>
+            <div className='flex flex-row '> 
+                {/* Upper part w/ img */}
+            </div>
+        </div>
+    );
+}
+
+export default Header;
