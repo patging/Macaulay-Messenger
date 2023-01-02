69b278735eface4fd7e13c7b49c2c1e03bc4e55b
amit397 <unconfigured@null.spigotmc.org>
2023-01-01 21:13:28 -0500

    worked on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,26 @@ export default function page() {
     return ( 
         <>
         <script src="https://cdn.tailwindcss.com"></script>
+        <section className="flex">
+          <div className="flex-none w-1/5 "></div>
+          <div className="flex-initial w-3/5">
+            <Header/>
+            <section className="grid grid-cols-2 gap-4 divide-x-2 divide-gray-200">
+              <div className="grid grid-rows-2 gap-4 divide-y divide-gray-200">
+                <div><ArticleThumbnail/></div> 
+                <div><ArticleThumbnail/></div>
+              </div>
+              <div className="grid grid-rows-2 gap-1 divide-y divide-gray-200">
+                <div><ArticleThumbnail/></div> 
+              </div>
+            </section>
+          </div>
+          <div className="flex-none w-1/5"></div>
+        </section>
         
-        <Header/>
-        <div className="grid grid-cols-3 divide-x">
-            <div><ArticleThumbnail/></div>
-            <div><ArticleThumbnail/></div>
-            <div><ArticleThumbnail/></div>
-        </div>
+        
+
+        
         
         <div className="border border-gray-200"></div>
         <h1 className="font-sans hover:font-serif">Hello, World!</h1>        
--- components/ArticleThumbnail.tsx
+++ components/ArticleThumbnail.tsx
@@ -4,20 +4,19 @@ import "../styles/links.css"
 
 const ArticleThumbnail = () => {
     return(  
-      
         <div className="group relative md:container md:mx-auto w-96 overflow-hidden rounded-[16px] p-[1px]">
           
           <div className="relative bg-white p-6">
               <div className="space-y-2">
-                <a href="yomama"><img src="https://media.zenfs.com/en/the_independent_635/52b7f58b061c788cf93123e099974227" alt=""/></a>
+                <a id="articleLink" href="yomama"><img id="articleImage" src="https://media.zenfs.com/en/the_independent_635/52b7f58b061c788cf93123e099974227" alt=""/></a>
                 
-                <a href="yodaday" className="text-2xl font-bold text-slate-800">Article Name</a>
+                <p id="articleTitle" className="text-2xl font-bold text-slate-800"><a id="articleLink" href="yodaday">Article Title</a></p>
 
-                <p className="text-sm font-semibold text-slate-800"><a href = "">Type</a></p>
-                <p className="text-base text-slate-800">Blah Blah Blah I Am a Government Drone Repeating Popular Opinions</p>
+                <p  className="text-sm font-semibold text-slate-800"><a id="typeArticle" href = "">Type</a></p>
+                <p id="synopsis" className="text-base text-slate-800">Blah Blah Blah I Am a Government Drone Repeating Popular Opinions</p>
                 <p className="text-sm font-semibold text-slate-400">
-                  <div>By <a href="https://www.youtube.com">Yo Mama</a> </div>
-                  <div>September 11, 2001</div>
+                  <div>By <a id="author" href="https://www.youtube.com">Yo Mama</a> </div>
+                  <div id="datePublished">September 11, 2001</div>
                 </p>
               </div>
         </div>
