dcebf67d36c30abdbd91ceb9c32ad64e60d08c7d
amit397 <unconfigured@null.spigotmc.org>
2023-01-03 16:21:43 -0500

    made masthead and side bar homepage

--- app/page.tsx
+++ app/page.tsx
@@ -10,26 +10,28 @@ import MastHead from '../components/masthead'
 
 import Footer from '../components/footer'
 
+import SideTab from '../components/sideTab_MastHead'
+
 // just here to test new file structure
 export default function page() {
     return ( 
         <>
 
          <script src="https://cdn.tailwindcss.com"></script>
-        <section className="flex">
+        
+      
+          <Header/>
+          <section className="flex flex-row divide-x-2 divide-gray-200">
+            <div className="basis-3/4"><MastHead/></div>
+            <div className="basis-1/4"><SideTab/></div>
+          </section>
           
-          <div className="flex-initial ">
-            <Header/>
-            <MastHead/>
-          </div>
-        </section>
-        
-        
 
-        
-        
-        <div className="border border-gray-200"></div>
-        <h1 className="font-sans hover:font-serif">Hello, World!</h1>      
+          <Footer/>
+          
+      
+          <div className="border border-gray-200"></div>
+          <h1 className="font-sans hover:font-serif">Hello, World!</h1>      
         </>
 
     );
