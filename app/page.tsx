f65df17f1f6dc379a9c95e7ec56757b7aa8b844a
patging <patrickging04@gmail.com>
2023-01-03 16:10:28 -0500

    Fixed merge confluct

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import "../styles/globals.css"
+import "../styles/global.css"
 
 import Header from "../components/Header"
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-<<<<<<< HEAD
       animation: {
         'spin-slow': 'spin 5s linear infinite',
       },
-=======
       fontFamily : {
         'playfair' : ['Playfair Display', 'serif'],
       },
 
->>>>>>> origin/main
     },
   },
   plugins: [],
