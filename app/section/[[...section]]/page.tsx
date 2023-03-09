314aeebfb606ce5721f0f1aeccad6bb4427a998e
amit397 <amitzxc123@gmail.com>
2023-03-08 20:48:42 -0500

    added api endpoints for header pages

--- app/section/[[...section]]/page.tsx
+++ app/section/[[...section]]/page.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
 import "../../../styles/global.css"
-
-import Header from "../../../components/Header"
+import Opinionpagearticle from '../../../components/Opinionpagearticle';
 
 export default async function Page({
     params,
@@ -33,24 +32,8 @@ export default async function Page({
             </div>
             <div className="divide-y-2 divide-gray-200">
                 <h2 className="text-1xl font-semibold pt-2 pb-2">Articles</h2>
-                <div className="flex pt-4">
-                    <div className="flex-intial text-gray">September 09, 2009</div>
-                    <div className="flex-auto pl-8 columns-1">
-                        <div className="text-2xl font-semibold"><a href="">Title</a></div>
-                        <div className="text-base">blurb</div>
-                        <div className="text-sm text-gray">By <a href="linkEffect">Author</a></div>
-                    </div>
-                    <div className="flex-none"><img src="https://pbs.twimg.com/profile_images/1108430392267280389/ufmFwzIn_400x400.png" alt="" /></div>
-                </div>
-                <div className="flex pt-4">
-                    <div className="flex-intial text-gray">September 09, 2009</div>
-                    <div className="flex-auto pl-8 columns-1">
-                        <div className="text-2xl font-semibold"><a href="">Title</a></div>
-                        <div className="text-base">blurb</div>
-                        <div className="text-sm text-gray">By <a href="linkEffect">Author</a></div>
-                    </div>
-                    <div className="flex-none"><img src="https://pbs.twimg.com/profile_images/1108430392267280389/ufmFwzIn_400x400.png" alt="" /></div>
-                </div>
+                <Opinionpagearticle {...articles[0]}/>
+                <Opinionpagearticle {...articles[1]}/>
 
             </div>
             
--- components/Opinionpagearticle.tsx
+++ components/Opinionpagearticle.tsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import "../styles/global.css"
+import "../styles/links.css"
+import Link from 'next/link'
 
-import "../../../styles/global.css"
-
-const Opinionpagearticle = () => {
-    
+export interface articleBody {
+  Title: string,
+  section: string,
+  description : string,
+  content: string,
+  authorID: number,
+  createdAt: string,
+  id: number,
 }
+
+
+const Opinionpagearticle = ({Title, description, section,  authorID, createdAt, id } : articleBody) => {
+    
+    
+    return (
+    <div className="flex pt-2 pb-4">
+        <div><p className="flex-intial text-gray">{createdAt} </p></div>
+        <div className="flex-auto pl-8 columns-1">
+            <Link href={`/article/${id}`} className="text-2xl font-semibold">{ Title.length < 100 ? Title : Title.substring(0,97) + "..." }</Link>
+            <p className="text-base">{description}</p>
+            <div className="text-sm text-gray">By <Link href={`/profile/${authorID}`}>{ `Author ${authorID}` }</Link></div>
+        </div>
+        <div className="flex-none"><img className="object-contain h-48 w-96" src="https://pbs.twimg.com/profile_images/1108430392267280389/ufmFwzIn_400x400.png" alt="" /></div>
+    </div>
+    )
+}
+
+export default Opinionpagearticle
